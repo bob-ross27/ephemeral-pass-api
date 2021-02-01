d12fb14aa2ad8f8120f91a7871da110aad983f9c
bob-ross27 <55351437+bob-ross27@users.noreply.github.com>
2021-02-01 09:28:46 -0400

    Additional tests. Alias entire script instead of just importing the FastAPI app.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,12 @@ import re
 
 from fastapi.testclient import TestClient
 
-from src.main import app
+import src.main as e_api
 
-client = TestClient(app)
+client = TestClient(e_api.app)
 
 
-def post_password():
+def post_password(views):
     """
     Post a test password.
     """
@@ -18,7 +18,7 @@ def post_password():
         "/api/new",
         json={
             "password": test_password,
-            "views": 1,
+            "views": views,
             "expiration": 1,
             "email_addresses": "user@example.com",
         },
@@ -40,15 +40,20 @@ def test_post_password():
     Create a new password and verify the successful response.
     """
 
-    response = post_password()
-    assert response.status_code == 200
-    assert re.match(
-        r'\{"Success":"Added Password at URL: [a-zA-Z0-9]{32}"\}',
+    response = post_password(1)
+    response_re = re.match(
+        r'\{"Success":"Added Password at URL: ([a-zA-Z0-9]{32})"\}',
         response.text,
     )
+    assert response.status_code == 200
+    assert response_re
+
+    get_password(
+        response_re[1]
+    )  # Remove password from DB. TODO: Move to DELETE once supported.
 
 
-def test_nonexistant_password():
+def test_get_nonexistant_password():
     """
     Invalid keys should return 404
     """
@@ -60,8 +65,11 @@ def test_nonexistant_password():
 
 
 def test_password_removed():
+    """
+    POST a new password, GET until view count exceeded and verify 404
+    """
 
-    post_response = post_password()
+    post_response = post_password(1)
     post_response = post_response.json()
     key = re.match(
         r"Added Password at URL: ([A-Za-z0-9]{32})", post_response["Success"]
@@ -72,3 +80,22 @@ def test_password_removed():
     get_response = get_password(key)
 
     assert get_response.status_code == 404
+
+
+def test_generate_secret_key():
+    """
+    Ensure secret key generation matches the expected format.
+    """
+    key = e_api.generate_secret_key(32)
+    assert re.match(r"^[a-zA-Z0-9]{32}$", key)
+
+
+def test_hash_key():
+    """
+    Ensure hashing the test secret key matches the expected value.
+    """
+
+    key = "ExampleSecretKey"
+    prehashed_key = b"\xcd\xda\x81h\xc4\xc8\xac\xd6m\xfa\xff~\xe9@\xc7\xf0\x17q\x9dX"
+    hashed_key = e_api.hash_key(key)
+    assert hashed_key == prehashed_key
